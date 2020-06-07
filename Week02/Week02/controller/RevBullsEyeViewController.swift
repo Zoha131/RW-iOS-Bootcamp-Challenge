fcb02ca892c75285066dc67a4328bfe1a0ecabc0
zoha131 <abirhasanzoha@gmail.com>
2020-06-07 10:32:53 +0600

    added revbullseye

--- Week02/Week02/controller/RevBullsEyeViewController.swift
+++ Week02/Week02/controller/RevBullsEyeViewController.swift
@@ -9,22 +9,86 @@
 import UIKit
 
 class RevBullsEyeViewController: UIViewController {
-
+    
+    var bullsEyeGame = BullsEyeGame(minTargetValue: 1, maxTargetValue: 100)
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var guessTxt: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var hitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
+        
+        slider.isEnabled = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func showAlert() {
+        
+        let difference = bullsEyeGame.difference
+        let points = bullsEyeGame.calculateScore()
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+        } else if difference < 5 {
+            title = "You almost had it!"
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: {
+            action in
+            self.bullsEyeGame.nextRound()
+            self.updateViews()
+        })
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
     }
-    */
 
+    
+    @IBAction func onTextChange(_ sender: Any) {
+        if  let text = self.guessTxt.text,
+            let roundedValue = Int(text),
+            roundedValue <= 100 && roundedValue >= 1
+        {
+            bullsEyeGame.currentValue = roundedValue
+            hitButton.isEnabled = true
+            
+        } else{
+            hitButton.isEnabled = false
+        }
+        
+    }
+    
+    func updateViews() {
+        slider.value = Float(bullsEyeGame.targetValue)
+        guessTxt.text = String(bullsEyeGame.currentValue)
+        scoreLabel.text = String(bullsEyeGame.totalScore)
+        roundLabel.text = String(bullsEyeGame.round)
+        
+
+        print(bullsEyeGame.targetValue)
+    }
+    
+    @IBAction func startNewGame() {
+        bullsEyeGame.reset()
+        updateViews()
+    }
+    
+    
+    
 }
