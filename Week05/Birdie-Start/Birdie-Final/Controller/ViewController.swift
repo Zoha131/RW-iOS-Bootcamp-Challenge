25af5aced12195734312a44becf68892573587bb
zoha131 <abirhasanzoha@gmail.com>
2020-06-29 13:42:52 +0600

    added image picker

--- Week05/Birdie-Start/Birdie-Final/Controller/ViewController.swift
+++ Week05/Birdie-Start/Birdie-Final/Controller/ViewController.swift
@@ -31,10 +31,71 @@ class ViewController: UIViewController {
   
   @IBAction func didPressCreateTextPostButton(_ sender: Any) {
     
+    openAlertView()
+    
+  }
+  
+  func openAlertView(image: UIImage? = nil) {
+    let alert = UIAlertController(title: "Create Post", message: "What's up? :]", preferredStyle: UIAlertController.Style.alert)
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler:nil)
+    
+    let okAction = UIAlertAction(title: "Ok", style: .default, handler:{ (UIAlertAction) in
+      
+      if let userName = alert.textFields?[0].text {
+        
+        let postTxt = alert.textFields?[1].text
+        let time = Date()
+        
+        if let postImage = image {
+          
+          let imagePost = ImagePost(textBody: postTxt, userName: userName, timestamp: time, image: postImage)
+          
+          self.postData.addImagePost(imagePost: imagePost)
+          
+          if let index = self.postData.mediaPosts.firstIndex(where: {$0.timestamp == imagePost.timestamp}) {
+            
+            let indexPath = IndexPath(row: index, section: 0)
+            self.tableview.insertRows(at: [indexPath], with: .automatic)
+          }
+          
+        } else {
+          let texstPost = TextPost(textBody: postTxt, userName: userName, timestamp: time)
+          
+          self.postData.addTextPost(textPost: texstPost)
+          
+          if let index = self.postData.mediaPosts.firstIndex(where: {$0.timestamp == texstPost.timestamp}) {
+            
+            let indexPath = IndexPath(row: index, section: 0)
+            self.tableview.insertRows(at: [indexPath], with: .automatic)
+          }
+        }
+      }
+    })
+    
+    alert.addTextField { (textField) in
+      textField.placeholder = "Username"
+    }
+    
+    alert.addTextField { (textField) in
+      textField.placeholder = "what's in your mind?"
+    }
+    
+    alert.addAction(okAction)
+    alert.addAction(cancelAction)
+    
+    self.present(alert, animated: true, completion: nil)
   }
   
   @IBAction func didPressCreateImagePostButton(_ sender: Any) {
     
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    picker.allowsEditing = false
+    picker.sourceType = .photoLibrary
+    picker.modalPresentationStyle = .fullScreen
+    
+    self.present(picker, animated: true, completion: nil)
   }
   
 }
@@ -64,6 +125,22 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
       return cell
     }
   }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    
+    picker.dismiss(animated: true, completion: nil)
+    
+    if let result = info[UIImagePickerController.InfoKey.originalImage],
+       let image = result as? UIImage{
+      
+      openAlertView(image: image)
+      
+    }
+  }
+  
 }
 
 
