27c81e0493f6251c7ae82ecb5a44fb1e68ab2072
zoha131 <abirhasanzoha@gmail.com>
2020-06-29 11:54:52 +0600

    configured table view with two custom cells

--- Week05/Birdie-Start/Birdie-Final/Controller/ViewController.swift
+++ Week05/Birdie-Start/Birdie-Final/Controller/ViewController.swift
@@ -9,26 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var tableview: UITableView!
+  
+  let postData = MediaPostsHandler.shared
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    postData.getPosts()
+    setUpTableView()
+  }
+  
+  func setUpTableView() {
+    // Set delegates, register custom cells, set up datasource, etc.
+    tableview.register(UINib(nibName: PostType.textPost.rawValue, bundle: nil), forCellReuseIdentifier: PostType.textPost.rawValue)
+    tableview.register(UINib(nibName: PostType.imagePost.rawValue, bundle: nil), forCellReuseIdentifier: PostType.imagePost.rawValue)
+    tableview.dataSource = self
+    tableview.delegate = self
+  }
+  
+  @IBAction func didPressCreateTextPostButton(_ sender: Any) {
+    
+  }
+  
+  @IBAction func didPressCreateImagePostButton(_ sender: Any) {
+    
+  }
+  
+}
 
-    @IBOutlet weak var tableview: UITableView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setUpTableView()
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return postData.mediaPosts.count
+  }
+  
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let item = postData.mediaPosts[indexPath.row]
+    
+    if let imagePost = item as? ImagePost {
+      
+      let cell = tableView.dequeueReusableCell(withIdentifier: PostType.imagePost.rawValue) as! ImagePostTableViewCell
+      cell.setData(imagePost: imagePost)
+      
+      return cell
+      
+    } else{
+      
+      let cell = tableView.dequeueReusableCell(withIdentifier: PostType.textPost.rawValue) as! TextPostTableViewCell
+      cell.setData(textPost: item)
+      
+      return cell
     }
-
-    func setUpTableView() {
-        // Set delegates, register custom cells, set up datasource, etc.
-    }
-
-    @IBAction func didPressCreateTextPostButton(_ sender: Any) {
-
-    }
-
-    @IBAction func didPressCreateImagePostButton(_ sender: Any) {
-
-    }
-
+  }
 }
 
 
